957c9c7cec9dc00487251065ef53ce5f53b121ee
Oliver Beattie <oliver@Oliver-Beatties-Mac-Pro.local>
2008-06-25 00:32:54 -1000

    From the Pro

--- photos/flickr/requests.py
+++ photos/flickr/requests.py
@@ -1,10 +1,40 @@
 """Flickr request classes."""
 import hashlib
 
+from django.conf import settings
+from django.utils.datastructures import SortedDict
 from django.utils.http import urlencode
 
-class FlickrParameters(dict):
-    """Subclass of the builtin dict type which """
+class FlickrParameters(SortedDict):
+    """Subclass of the builtin dict type which helps in generating
+       the parameters to pass in a Flickr request."""
+    def __init__(self, data={}, *args, **kwargs):
+        data.update({ 'api_key': settings.FLICKR_API_KEY })
+        return super(FlickrParameters, self).__init__(data, *args, **kwargs)
     
-
-class FlickrAuthenticationDetails(object):
+    def sorted_items(self):
+        """Sorts self.items() in ascending order and then returns the result."""
+        def item_compare(x, y):
+            if x[0] > y[0]:
+                return 1
+            elif x[0] == y[0]:
+                return 0
+            else:
+                return -1
+        
+        return sorted(self.items(), item_compare)
+    
+    def hashed(self):
+        return hashlib.md5(u'%(secret)s%(params)s' % {
+            'secret': settings.FLICKR_API_SECRET,
+            'params': ''.join([u'%s%s' % (i[0], i[1]) for i in self.sorted_items()]),
+        }).hexdigest()
+    
+    def urlencode(self):
+        """Returns the urlencoded result of the parameters, ready to append as a query
+           string."""
+        parameters = SortedDict()
+        for param in self.sorted_items():
+            parameters[param[0]] = param[1]
+        parameters['api_sig'] = self.hashed()
+        return urlencode(parameters)
--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = 'obeattie'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'django'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'Calif0rni4!'         # Not used with sqlite3.
+DATABASE_USER = 'postgres'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'postgres'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
